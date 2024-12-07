aa1f0c96fa011dff0658fc5383a19bef39cba56d
D4NGK4 <danjosheps.djt98@gmail.com>
2024-12-08 00:36:49 +0800

    added instance attributes for base entity

--- oop_practice.py
+++ oop_practice.py
@@ -1,3 +1,12 @@
 class base_entity:
-    pass
+
+    def __init__(self, entity_name, entity_species, entity_class, entity_health, entity_mana):
+        self.entity_name = entity_name
+        self.entity_species = entity_species
+        self.entity_class = entity_class
+        self.entity_health = entity_health
+        self.entity_mana = entity_mana
+
+
+
     
