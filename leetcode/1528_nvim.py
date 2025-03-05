4329a3c191cfe61fb5eb9aca8c346bb3ae5adc0d
D4NGK4 <danjosheps.djt98@gmail.com>
2025-03-05 14:41:48 +0800

    new problem = new problems

---
+++ leetcode/1528_nvim.py
@@ -0,0 +1,12 @@
+s = "codeleet"
+indices = [4, 5, 6, 7, 0, 2, 1, 3]
+
+s = list(s)
+
+dictionary = {}
+
+for i in range(len(indices)):
+    item = {s[i]: indices[i]}
+    dictionary.append(item)
+
+print(dictionary)
