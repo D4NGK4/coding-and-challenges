49c844ee2c789771d903b43dc20a4ea4d61a6ac6
D4NGK4 <danjosheps.djt98@gmail.com>
2025-03-06 13:44:31 +0800

    finished 1528

--- leetcode/1528_nvim.py
+++ leetcode/1528_nvim.py
@@ -1,11 +1,13 @@
 s = "codeleet"
 indices = [4, 5, 6, 7, 0, 2, 1, 3]
 
+t = len(indices)
+ans = ""
 s = list(s)
 
-dictionary = {}
+for i in range(t):
+    for j in range(len(indices)):
+        if i == indices[j]:
+            ans += s[j]
 
-
-for i in range(len(indices)):
-    item = {s[i]: indices[i]}
-    dictionary.append(item)
+print(ans)
